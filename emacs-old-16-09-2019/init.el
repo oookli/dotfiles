92a56a30dbdc4baeea26a55cfd84be4dccc1ec04
Ilko Fedak <ilko.f@matic.com>
2019-10-29 19:42:04 +0200

    some changes inside emacs config

---
+++ emacs-old-16-09-2019/init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file (expand-file-name "configuration.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 (quote
+		("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+	 (quote
+		(sublimity minimap minimap-mode robe robe-mode moody evil general which-key use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "gray10" :foreground "dark gray" :box nil :weight normal :height 1.0 :width normal :underline nil :overline nil))))
+ '(mode-line-inactive ((t (:background "gray10" :foreground "dark gray" :box nil :weight normal :height 1.0 :width normal :underline nil :overline nil)))))
--- emacs/init.el
+++ emacs/init.el
@@ -1,19 +1,90 @@
-(org-babel-load-file (expand-file-name "configuration.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-	 (quote
-		("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(package-selected-packages
-	 (quote
-		(sublimity minimap minimap-mode robe robe-mode moody evil general which-key use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "gray10" :foreground "dark gray" :box nil :weight normal :height 1.0 :width normal :underline nil :overline nil))))
- '(mode-line-inactive ((t (:background "gray10" :foreground "dark gray" :box nil :weight normal :height 1.0 :width normal :underline nil :overline nil)))))
+;;; -*- lexical-binding: t -*-
+
+(defun tangle-init ()
+  "If the current buffer is 'init.org' the code-blocks are
+tangled, and the tangled file is compiled."
+  (when (equal (buffer-file-name)
+               (expand-file-name (concat user-emacs-directory "init.org")))
+    ;; Avoid running hooks when tangling.
+    (let ((prog-mode-hook nil))
+      (org-babel-tangle)
+      (byte-compile-file (concat user-emacs-directory "init.el")))))
+
+(add-hook 'after-save-hook 'tangle-init)
+
+(eval-and-compile
+  (setq load-prefer-newer t
+        package-user-dir "~/.emacs.d/elpa"
+        package--init-file-ensured t
+        package-enable-at-startup nil)
+
+  (unless (file-directory-p package-user-dir)
+    (make-directory package-user-dir t))
+
+  (setq load-path (append load-path (directory-files package-user-dir t "^[^.]" t))))
+
+
+(eval-when-compile
+  (require 'package)
+  ;; tells emacs not to load any packages before starting up
+  ;; the following lines tell emacs where on the internet to look up
+  ;; for new packages.
+  (setq package-archives '(("melpa"        . "https://melpa.org/packages/")
+                           ("melpa-stable" . "http://stable.melpa.org/packages/")
+                           ("elpa"         . "https://elpa.gnu.org/packages/")
+                           ("repo-org"     . "https://orgmode.org/elpa/")
+                           ("org"          . "http://orgmode.org/elpa/")
+                           ("marmalade"    . "http://marmalade-repo.org/packages/")
+                           ("elpy"         . "https://jorgenschaefer.github.io/packages/")))
+  ;; (package-initialize)
+  (unless package--initialized (package-initialize t))
+
+  ;; Bootstrap `use-package'
+  (unless (package-installed-p 'use-package) ; unless it is already installed
+    (package-refresh-contents) ; updage packages archive
+    (package-install 'use-package)) ; and install the most recent version of use-package
+
+  (require 'use-package)
+  (setq use-package-always-ensure t))
+
+(setq mac-right-command-modifier 'super)
+(setq mac-option-modifier 'meta)
+(setq mac-left-option-modifier 'meta)
+(setq mac-right-option-modifier 'meta)
+(setq mac-command-modifier 'super)
+
+(setq mac-right-option-modifier 'nil)
+
+(set-face-background 'show-paren-match "grey84")
+(set-face-attribute 'show-paren-match nil :weight 'extra-bold)
+(show-paren-mode)
+
+;; (global-visual-line-mode 1)
+(setq org-startup-truncated nil)
+
+(setq column-number-mode t) ;; show columns in addition to rows in mode line
+
+(setq-default frame-title-format "%b (%f)")
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+(setq-default c-basic-offset 2)
+(setq c-basic-offset 2)
+(setq-default tab-width 2)
+(setq-default c-basic-indent 2)
+
+(blink-cursor-mode 0)
+
+(global-visual-line-mode t)
+
+(use-package evil
+  :hook (after-init . evil-mode)
+  :init
+  (setq
+    evil-want-C-u-scroll t
+    ;; don't move back the cursor one position when exiting insert mode
+    evil-move-cursor-back nil)
+  :config
+    (setq doc-view-continuous t)
